eeb1ad4cf3c71a8548d37746644ce6d5ce9a7fa8
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-11-08 16:17:25 +0100

    Add logging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,7 +1,11 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir radoslaw.szymczyszyn@erlang-solutions.com"} ]
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,9 +17,12 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     Poison.decode(body)
   end
-  def handle_response({:ok, %{status_code: ___, body: body}}) do
+  def handle_response({:ok, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
     {:error, Poison.decode(body)}
   end
 end
